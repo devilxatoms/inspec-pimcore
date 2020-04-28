24d087306928b6f1b33a0f5fea6e7aa167630c10
Brayan Caldera <brayan.caldera@softtek.com>
2020-04-28 03:03:16 -0500

    [add](inspec) check nano package

--- controls/checkPackages.rb
+++ controls/checkPackages.rb
@@ -4,3 +4,15 @@ end
 describe package('php-pear') do
     it { should be_installed }
 end
+describe package('apache2') do
+    it { should be_installed }
+end
+describe package('libapache2-mod-php7.4') do
+    it { should be_installed }
+end
+describe package('mysql-client') do
+    it { should be_installed }
+end
+describe package('nfs-common') do
+    it { should be_installed }
+end
