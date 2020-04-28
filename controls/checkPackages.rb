c64d9188cd8420335bd98312ca3b739ca4eddf0e
Brayan Caldera <brayan.caldera@softtek.com>
2020-04-28 02:14:37 -0500

    [add](inspec) check nano package

--- controls/checkPackages.rb
+++ controls/checkPackages.rb
@@ -1,10 +1,3 @@
-case os[:family]
-when 'ubuntu'
-  describe package('nano') do
+describe package('nano') do
     it { should be_installed }
-  end
-when 'redhat'
-  describe package('mysql-client') do
-    it { should be_installed }
-  end
 end
