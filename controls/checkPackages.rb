94baf37b2c085fd5d46afe4c26a1117bb325934a
Brayan Caldera <brayan.caldera@softtek.com>
2020-04-28 12:31:52 -0500

    [add](inspec) check nano package

--- controls/checkPackages.rb
+++ controls/checkPackages.rb
@@ -1,35 +1,39 @@
-describe package('php7.4') do
-    it { should be_installed }
-end
-describe package('php-pear') do
-    it { should be_installed }
-end
-describe package('apache2') do
-    it { should be_installed }
-end
-describe package('libapache2-mod-php7.4') do
-    it { should be_installed }
-end
-describe package('mysql-client') do
-    it { should be_installed }
-end
-describe package('nfs-common') do
-    it { should be_installed }
-end
-describe package('git') do
-    it { should be_installed }
-end
-describe package('binutils') do
-    it { should be_installed }
-end
-describe package('make') do
-    it { should be_installed }
-end
-describe package('ffmpeg') do
-    it { should be_installed }
-end
-describe package('php7.4-gd') do
-    it { should be_installed }
+control 'verify-packages' do
+    impact 1
+    title 'system is up-to-date'
+    describe package('php7.4') do
+        it { should be_installed }
+    end
+    describe package('php-pear') do
+        it { should be_installed }
+    end
+    describe package('apache2') do
+        it { should be_installed }
+    end
+    describe package('libapache2-mod-php7.4') do
+        it { should be_installed }
+    end
+    describe package('mysql-client') do
+        it { should be_installed }
+    end
+    describe package('nfs-common') do
+        it { should be_installed }
+    end
+    describe package('git') do
+        it { should be_installed }
+    end
+    describe package('binutils') do
+        it { should be_installed }
+    end
+    describe package('make') do
+        it { should be_installed }
+    end
+    describe package('ffmpeg') do
+        it { should be_installed }
+    end
+    describe package('php7.4-gd') do
+        it { should be_installed }
+    end
 end
 
 describe command('php -m | grep gd') do
