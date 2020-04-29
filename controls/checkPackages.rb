cb8d18bf83374d327dd5e7f9f80502bb2868caf1
Brayan Caldera <brayan.caldera@softtek.com>
2020-04-28 20:54:45 -0500

    [add](inspec) add mariadb client validation

--- controls/checkPackages.rb
+++ controls/checkPackages.rb
@@ -91,7 +91,7 @@ control 'check-php-packages' do
     describe package('php7.4-mbstring') do
         it { should be_installed }
     end
-    describe package('php-mysqlnd') do
+    describe package('php7.4-mysqlnd') do
         it { should be_installed }
     end
     describe package('php-redis') do
@@ -100,7 +100,7 @@ control 'check-php-packages' do
     describe package('php7.4-zip') do
         it { should be_installed }
     end
-    describe package('simplexml') do
+    describe package('php7.4-simplexml') do
         it { should be_installed }
     end
     describe package('php7.4-bcmath') do
