3515da6b035492828f00964558495a4f179f656d
Brayan Caldera <brayan.caldera@softtek.com>
2020-04-28 19:17:28 -0500

    [add](inspec) add mariadb client validation

--- controls/checkPackages.rb
+++ controls/checkPackages.rb
@@ -13,7 +13,7 @@ control 'check-system-packages' do
     describe package('libapache2-mod-php7.4') do
         it { should be_installed }
     end
-    describe package('mysql-client') do
+    describe package('mariadb-client') do
         it { should be_installed }
     end
     describe package('nfs-common') do
